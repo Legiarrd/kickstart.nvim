95532c1bfc3444994e745e036abd714f28b24dc2
Felix <legiarrd1337@icloud.com>
2024-04-24 15:39:38 +0200

    plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    { 'wakatime/vim-wakatime', lazy = false },
+    { 'github/copilot.vim' },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons",
+          "MunifTanjim/nui.nvim",
+          "3rd/image.nvim",
+        }
+    },
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        config = function()
+          require("tailwindcss-colorizer-cmp").setup({
+            color_square_width = 2,
+          })
+        end
+      },
+    {'akinsho/toggleterm.nvim', version = "*", config = true}
+}
