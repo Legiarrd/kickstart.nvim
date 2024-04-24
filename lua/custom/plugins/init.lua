cbd193f3b2b02bade81e397bb95272df59a70868
Felix <legiarrd1337@icloud.com>
2024-04-24 16:07:03 +0200

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,23 +5,35 @@
 return {
     { 'wakatime/vim-wakatime', lazy = false },
     { 'github/copilot.vim' },
+    {'nvim-tree/nvim-web-devicons', enabled = true},
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
         dependencies = {
           "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
           "MunifTanjim/nui.nvim",
-          "3rd/image.nvim",
+          "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
     },
     {
         "roobert/tailwindcss-colorizer-cmp.nvim",
+        -- optionally, override the default options:
         config = function()
           require("tailwindcss-colorizer-cmp").setup({
             color_square_width = 2,
           })
         end
       },
-    {'akinsho/toggleterm.nvim', version = "*", config = true}
+    {'akinsho/toggleterm.nvim', version = "*", config = true},
+    {
+      'nvimdev/dashboard-nvim',
+      event = 'VimEnter',
+      config = function()
+        require('dashboard').setup {
+          -- config
+        }
+      end,
+      dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    },
 }
